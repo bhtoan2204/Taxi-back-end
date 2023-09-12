bf9f74cf79f2aa65d120b2d53c98c2922fecc625
Bành Hảo Toàn <86637323+bhtoan2204@users.noreply.github.com>
2023-09-12 17:51:41 +0700

    deletion

--- apps/apps/revenue/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { RevenueModule } from './revenue.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(RevenueModule);
-  await app.listen(3000);
-}
-bootstrap();
